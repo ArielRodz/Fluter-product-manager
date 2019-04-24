34bc81e6caf2fe57154381f3a509356dd8e5abec
Ariel Chepito <arizul96@gmail.com>
2019-04-24 16:00:27 -0600

    StatefulWidget and Stateles Widget example

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import './product_manager.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Essy List'),
+          ),
+          body: ProductManager(),
+      ),
+    );
+  }
+
+}
+
